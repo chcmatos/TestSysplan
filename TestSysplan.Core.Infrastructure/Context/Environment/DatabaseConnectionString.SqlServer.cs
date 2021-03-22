381fbccbfdb8f191de127b8f5fb201704d0de6fb
Carlos Matos <chcmatos@hotmail.com>
2021-03-21 22:33:36 -0300

    ...

--- TestSysplan.Core.Infrastructure/Context/Environment/DatabaseConnectionString.SqlServer.cs
+++ TestSysplan.Core.Infrastructure/Context/Environment/DatabaseConnectionString.SqlServer.cs
@@ -1,4 +1,6 @@
-﻿namespace TestSysplan.Core.Infrastructure.Context.Environment
+﻿using System.Text;
+
+namespace TestSysplan.Core.Infrastructure.Context.Environment
 {
     internal sealed class DatabaseConnectionStringSqlServer : DatabaseConnectionString
     {
@@ -6,14 +8,17 @@
 
         protected override string GetConnectionString(ConfigEnvironment env)
         {
-            return 
-                $"Server={env.DatabaseHost.GetValueOrDefault(".")},{env.DatabasePort.GetValueOrDefault(1433)};" +
-                $"Database={env.DatabaseName};" +
-                $"User Id={env.DatabaseUser.GetValueOrDefault("sa")};" +
-                $"Password={env.DatabasePswd};" +
-                $"Integrated Security={!ConfigEnvironment.DotnetRunningInContainer};" +
-                $"MultipleActiveResultSets=True;" +
-                $"Connection Timeout={env.DatabaseTout.GetValueOrDefault(30)}";
+            return new StringBuilder()
+                .Append("Data Source=").Append(env.DatabaseHost.GetValueOrDefault("."))
+                .Append(",").Append(env.DatabasePort.GetValueOrDefault(1433)).Append(";")
+                .Append("Initial Catalog=").Append(env.DatabaseName.Value).Append(";")
+                .AppendIf(env.DatabaseUser.HasValue, "User Id=", env.DatabaseUser.Value, ';')
+                .AppendIf(env.DatabasePswd.HasValue, "Password=", env.DatabasePswd.Value, ';')
+                .AppendIf((!env.DatabaseUser.HasValue && !env.DatabasePswd.HasValue) || !ConfigEnvironment.DotnetRunningInContainer, "Integrated Security=True;")
+                .AppendIf((!env.DatabaseUser.HasValue && !env.DatabasePswd.HasValue), "Trusted_Connection=True;")
+                .Append("MultipleActiveResultSets=True;")
+                .Append("Connection Timeout=").Append(env.DatabaseTout.GetValueOrDefault(30)).Append(";")
+                .ToString();
         }
     }
 }
