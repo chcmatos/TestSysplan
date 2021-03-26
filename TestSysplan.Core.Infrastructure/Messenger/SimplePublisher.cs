85d18206fdd3d63ad18723141a6e03456ee55e12
Carlos Matos <chcmatos@hotmail.com>
2021-03-26 12:17:06 -0300

    ...

--- TestSysplan.Core.Infrastructure/Messenger/SimplePublisher.cs
+++ TestSysplan.Core.Infrastructure/Messenger/SimplePublisher.cs
@@ -1,7 +1,8 @@
 ﻿using RabbitMQ.Client;
 using System;
+using System.Linq;
+using System.Text;
 using System.Text.Json;
-using System.Threading;
 
 namespace TestSysplan.Core.Infrastructure.Messenger
 {
@@ -21,19 +22,16 @@ namespace TestSysplan.Core.Infrastructure.Messenger
 
         private SimplePublisher() { }
 
-        public void SendMessage<TModel>(TModel target, string queueName = null, string exchange = "")
+        #region SendMessage
+        public void SendMessage(byte[] message, string queueName)
         {
-            if (target is null)
+            if (message is null)
             {
-                throw new ArgumentException(nameof(target));
+                throw new ArgumentNullException(nameof(message));
             }
-            else if(queueName is null)
+            else if (string.IsNullOrWhiteSpace(queueName))
             {
-                queueName = nameof(TModel);
-            }
-            else if (queueName.Trim().Length == 0)
-            {
-                throw new ArgumentException("Invalid queue name!");
+                throw new ArgumentNullException(nameof(queueName));
             }
 
             using (var conn = AmqpConnectionWrapper.GetInstance())
@@ -46,21 +44,94 @@ namespace TestSysplan.Core.Infrastructure.Messenger
                     autoDelete: false,
                     arguments: null);
 
-                byte[] json = JsonSerializer.SerializeToUtf8Bytes(target,
-                    new JsonSerializerOptions
-                    {
-                        IgnoreReadOnlyFields = true,
-                        IgnoreReadOnlyProperties = true,
-                        IgnoreNullValues = true
-                    });
+                channel.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey: queueName,
+                    basicProperties: null,
+                    body: message);
+            }
+        }
+
+        public void SendMessage(string message, string queueName, Encoding encoding = null)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+
+            encoding ??= Encoding.Default;
+            SendMessage(encoding.GetBytes(message), queueName);
+        }
+
+        public void SendMessage<TModel>(TModel target, string queueName = null)
+        {
+            if (target is null)
+            {
+                throw new ArgumentException(nameof(target));
+            }
+            else if (queueName is null)
+            {
+                queueName = nameof(TModel);
+            }
+            else if (queueName.Trim().Length == 0)
+            {
+                throw new ArgumentException("Invalid queue name!");
+            }
+
+            var json = JsonSerializer.SerializeToUtf8Bytes(target, new JsonSerializerOptions
+            {
+                IgnoreReadOnlyFields = true,
+                IgnoreReadOnlyProperties = true,
+                IgnoreNullValues = true
+            });
+
+            SendMessage(json, queueName);
+        }
+        #endregion
+
+        public void SendMessageExchangeFanout(byte[] message, string exchange, params string[] queues)
+        {
+            if (message is null)
+            {
+                throw new ArgumentNullException(nameof(message));
+            }
+            else if (queues.Length == 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(queues));
+            }
+            else if (string.IsNullOrWhiteSpace(exchange))
+            {
+                throw new ArgumentNullException(nameof(exchange));
+            }
+
+            using (var conn = AmqpConnectionWrapper.GetInstance())
+            using (var channel = conn.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange, "fanout");
+
+                foreach(var queue in queues)
+                {
+                    channel.QueueDeclare(
+                    queue: queue,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                    channel.QueueBind(queue, exchange, string.Empty);
+                }
 
                 channel.BasicPublish(
                     exchange: exchange,
-                    routingKey: queueName,
+                    routingKey: string.Empty,
                     basicProperties: null,
-                    body: json);
-                Thread.Sleep(200);
+                    body: message);
             }
+        }
+
+        public void SendMessageExchangeFanout(byte[] message, params string[] queues)
+        {
+            this.SendMessageExchangeFanout(message, string.Join('_', queues), queues);
         }
     }
 }
